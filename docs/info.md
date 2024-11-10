88625a6f8892e7d1c100df9b00a72b3259c66934
Tommy Thorn <tommy-github2@thorn.ws>
2024-11-10 09:26:10 -0800

    Ready for TO?

--- docs/info.md
+++ docs/info.md
@@ -9,12 +9,30 @@ You can also include images in this folder and reference them in the markdown. E
 
 ## How it works
 
-Explain how your project works
+(This is a variant of tt06-ncl-lfsr, but with different C-gate
+implementations)
+
+Muller's C-gate is a state-holding element with two inputs A and B,
+and an output Q.  Q holds the previous state unless A == B in which
+case it takes on this value.  There are many ways to implement the
+C-gate.  In this design, we try two: building it from a latch and
+building it out of combinatorial logic.  The two inputs ui[0] and
+ui[1] are fed to two C-gates Cl and Cc, build with a latch and
+combinatorial logic respectively.  Their respective outputs are wired
+to uo[0] and uo[1].
+
+We also build four rings from this, with uo[2] and uo[3] being the
+output of a four stage build from Cl and Cc gates respectively.
+Similar for uo[4]/uo[5] except using 16 stage rings and uo[6]/uo[7]
+for (TBD) stage rings.
 
 ## How to test
 
-Explain how to use your project
+Set ui[0] and ui[1] different values and verify that uo[0]/uo[1] only
+changes when both agree.  Observe uo[7:2] and look for transitions.
 
 ## External hardware
 
-List external hardware used in your project (e.g. PMOD, LED display, etc), if any
+For the basic test the rp2040 on the bringup board should be enough
+for the ring test, an oscilloscope is [probably] required.
+
--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_tommythorn_cgates (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,85 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
+   wire	A = ui_in[0];
+   wire	B = ui_in[1];
+   wire	Ql, Qc, R4l, R4c, R16l, R16c, RTBDl, RTBDc;
+
+   assign uo_out = {RTBDc, RTBDl, R16c, R16l, R4c, R4l, Qc, Ql};
+   latchcgate latchcgate_inst(rst_n, A, B, Ql);
+   combcgate combcgate_inst(rst_n, A, B, Qc);
+
+   latchring #(4) r4li(rst_n, R4l);
+   combring  #(4) r4ci(rst_n, R4c);
+   latchring #(16) r16li(rst_n, R16l);
+   combring  #(16) r16ci(rst_n, R16c);
+   latchring #(16) rtbdli(rst_n, RTBDl);
+   combring  #(16) rtbdci(rst_n, RTBDc);
+endmodule
+
+module latchcgate #(parameter q0 = 0) (input rst_n, input A, input B, output reg Q);
+   always @* if (!rst_n) Q = q0; else if (A == B) Q = #3 A;
+endmodule
+
+module combcgate #(parameter q0 = 0) (input rst_n, input A, input B, output reg Q);
+   always @* Q = #3 rst_n ? A & B | (A | B) & Q : q0;
+endmodule
+
+module latchring #(parameter N = 3) (input rst_n, output Q);
+   wire [N-1:0] o;
+   assign Q = o[0];
+   genvar	i;
+   for (i = 0; i < N; i = i + 1) begin
+      latchcgate #(i == 0) latchcgate_inst(.rst_n(rst_n),
+				   .A(o[(i + N - 1) % N]),
+				   .B(!o[(i + 1) % N]),
+				   .Q(o[i]));
+   end
+endmodule
+
+module combring#(parameter N = 3) (input rst_n, output Q);
+   wire [N-1:0] o;
+   assign Q = o[0];
+   genvar	i;
+   for (i = 0; i < N; i = i + 1) begin
+      latchcgate #(i == 0) combcgate_inst(.rst_n(rst_n),
+				  .A(o[(i + N - 1) % N]),
+				  .B(!o[(i + 1) % N]),
+				  .Q(o[i]));
+   end
+endmodule
+
+module tb;
+   reg  [7:0] ui_in = 0;
+   wire [7:0] uo_out;
+   wire [7:0] uio_in;
+   wire [7:0] uio_out;
+   wire [7:0] uio_oe;
+   wire	      ena;
+   reg	      clk = 1;
+   reg	      rst_n = 0;
+
+   tt_um_tommythorn_cgates inst(ui_in, uo_out, uio_in, uio_out, uio_oe, ena, clk, rst_n);
+
+   initial begin
+      $monitor("%05d  %02x", $time, uo_out);
+      #10 rst_n = 1;
+      $display("Expect uo[1:0] to remain low");
+      #10 ui_in[1] = 1;
+      #10 ui_in[0] = 0;
+      #10 ui_in[1] = 1;
+      $display("Expect uo[1:0] to go high");
+      #10 ui_in[0] = 1;
+      #10 ui_in[1] = 0;
+      $display("Expect uo[1:0] to go low");
+      #10 ui_in[0] = 0;
+
+      #1000 $finish;
+   end
 endmodule
